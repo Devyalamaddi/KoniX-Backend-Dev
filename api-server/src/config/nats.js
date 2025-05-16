5f2314f315bd94c70eb06f2f3faef3a48c34f9a7
Devendra Yalamaddi <devendrayalamaddi@gmail.com>
2025-05-16 23:03:32 +0530

    feat(api): Add configuration setup for API server

---
+++ api-server/src/config/nats.js
@@ -0,0 +1,40 @@
+const { connect: natsConnect, StringCodec } = require('nats');
+const { logger } = require('../utils/logger');
+const { storeCryptoStats } = require('../services/cryptoService');
+
+let nc = null;
+
+const connect = async () => {
+  try {
+    nc = await natsConnect({ servers: process.env.NATS_URL });
+    logger.info('Connected to NATS');
+
+    // Subscribe to crypto stats update events
+    const sc = StringCodec();
+    const sub = nc.subscribe('crypto.stats.update');
+    
+    (async () => {
+      for await (const m of sub) {
+        const data = JSON.parse(sc.decode(m.data));
+        if (data.trigger === 'update') {
+          logger.info('Received update trigger from worker');
+          await storeCryptoStats();
+        }
+      }
+    })().catch((error) => {
+      logger.error('NATS subscription error:', error);
+    });
+
+    return nc;
+  } catch (error) {
+    logger.error('NATS connection error:', error);
+    throw error;
+  }
+};
+
+const getConnection = () => nc;
+
+module.exports = {
+  connect,
+  getConnection
+}; 
